37036c254f44771bbd8500316b601714b2a91a1a
Joao Azevedo <62391524+Azeved00@users.noreply.github.com>
2022-09-14 11:59:45 +0100

    trying to add config files automatically

--- Bin/link.sh
+++ Bin/link.sh
@@ -3,14 +3,24 @@ echo "linking config files"
 #creating symlinks to where the config files should be
 #while moving the alredy existing ones to the Backup folder
 
-#append path to core to bashrc
+#bashrc 
 if [ -f ~/.bashrc ]; then
 	mv ~/.bashrc $DotFilesFolder/Backup/
 fi
 ln -s $DotFilesFolder/RC/bash ~/.bashrc
 
-
+#inputrc
 if [ -f ~/.inputrc ]; then	
 	mv ~/.inputrc $DotFilesFolder/Backup/
 fi
 ln -s $DotFilesFolder/RC/input ~/.inputrc
+
+#nvim
+if ! [ -d ~/.config/nvim/ ]; then
+	mkdir -p ~/.config/nvim
+fi
+
+if  [ -f ~/.config/nvim/init.vim ]; then
+	mv ~/.config/nvim/init.vim $DotFilesFolder/Backup/nvimConfig.vim	
+fi
+ln -s $DotFilesFodler/Config/nvim/init.vim ~/.config/nvim/init.vim
