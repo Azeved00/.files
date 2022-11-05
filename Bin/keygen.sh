a2ee5418d155bcad49ab33f6b25526cba8ace127
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2022-11-05 18:33:42 +0000

    initiated the progress for generating keys

--- Bin/keygen.sh
+++ Bin/keygen.sh
@@ -1,7 +1,16 @@
 echo "generating key"
 
+#here i still need to do some work
+# like get how many keys i want to generate
+# the email
+# the name
+# possibly the type
+# and
+
 ssh-keygen -t ed25519 -C "jferazevedo@gmail.com"
-eval("$(ssh-agent -s)")
+
+
+eval $(ssh-agent -s) 
 ssh-add ~/.ssh/id_ed25519
 
 cat ~/.ssh/id_ed25519.pub
