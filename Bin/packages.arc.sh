715b81d85562de85c0d6620e6edb3370ee3d5009
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2023-01-25 19:24:56 +0000

    added some basic packages for arch

--- Bin/packages.arc.sh
+++ Bin/packages.arc.sh
@@ -1,3 +1,14 @@
 sudo pacman -Syu
 
 #install whatever i need here
+sudo pacman -S polybar
+sudo pacman -S feh
+sudo pacman -S yay
+sudo pacman -S rofi
+sudo pacman -S picom
+sudo pacman -S firefox
+sudo pacman -S alacritty
+#install xrandr
+#install pulseaudio
+
+yay -S betterlockscreen
