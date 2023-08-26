e4d6c7d53b190271426f062e4fed6b95fe20c26d
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2023-08-26 18:03:27 +0100

    changed sessionVariables to a better thing

--- NixOs/modules/variables.nix
+++ NixOs/modules/variables.nix
@@ -1,8 +1,6 @@
 {...}:
 {
     environment.sessionVariables = {  
-        TERMINAL = "alacritty";
-        #EDITOR = "nvim";
         NIX_CONF = "/etc/nixos/nix.conf";
         XDG_CONFIG_HOME = "$HOME/.config";
     };
