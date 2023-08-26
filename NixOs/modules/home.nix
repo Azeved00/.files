e3b0a3d9eec865fee4e10ce71f539b5ed247ff41
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2023-08-26 19:12:56 +0100

    migrated ssh config

--- NixOs/modules/home.nix
+++ NixOs/modules/home.nix
@@ -111,6 +111,39 @@
             };
         };
 
+        programs.ssh = {
+            enable = true;
+
+            matchBlocks = {
+                "github.com" = {
+                    hostname = "github.com";
+                    user="git";
+                    identityFile = "~/.ssh/main";
+                    extraOptions={
+                        AddKeysToAgent = "yes";
+                    };
+                };
+
+                "second" = {
+                    hostname = "github.com";
+                    user = "git";
+                    identityFile="~/.ssh/second";
+                    extraOptions={
+                        AddKeysToAgent = "yes";
+                    };
+                };
+
+                "gitlab" = {
+                    hostname = "gitedu.alunos.dcc.fc.up.pt";
+                    user = "git";
+                    identityFile="~/.ssh/gitlab";
+                    extraOptions={
+                        AddKeysToAgent = "yes";
+                    };
+                };
+            };
+        };
+
         programs.home-manager.enable = true;
    };
 }
