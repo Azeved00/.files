1701b08e2f29efb647fd48c37b39c2da27f9ef3e
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2023-08-30 17:02:53 +0100

    updated how link is created

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -1,12 +1,12 @@
 { config, pkgs, lib, ...}:
 {
-xdg.enable = true;
-xdg.configFile."nvim" =  {
-	enable = true;
-	recursive = false;
-	source = ../Config/nvim;
-	target = "nvim";
-};
+home.activation.installNvimConfig = ''
+    if [ -d "${config.home.homeDirectory}/.config/nvim" ]; then
+        rm ${config.home.homeDirectory}/.config/nvim
+    fi
+    ln -s "${config.home.homeDirectory}/.files/Config/nvim" "${config.home.homeDirectory}/.config/nvim"
+'';
+
 
 programs.neovim = {
     enable = true;
