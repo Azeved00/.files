7dca8bf8b2b4a89140d55c9bb7c79e3dcf03c848
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2023-08-31 12:36:29 +0100

    added lsp for lua and cpp + moved tresitter config

--- Config/nvim/after/plugin/lsp.lua
+++ Config/nvim/after/plugin/lsp.lua
@@ -22,28 +22,21 @@ local on_attach = function(_, bufnr)
     end, {})
 end
 
-
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
--- mason
---require("mason").setup()
---require("mason-lspconfig").setup_handlers({
-
---  function(server_name)
---        require("lspconfig")[server_name].setup {
---            on_attach = on_attach,
---            capabilities = capabilities
---        }
---    end,
-
---    ["lua_ls"] = function()
---        require('lspconfig').lua_ls.setup {
---            on_attach = on_attach,
---            capabilities = capabilities,
---            Lua = {
- --             workspace = { checkThirdParty = false },
- --             telemetry = { enable = false },
---            },
---        }
---    end
---})
+require('lspconfig').lua_ls.setup {
+     on_attach = on_attach,
+     capabilities = capabilities,
+     Lua = {
+       workspace = { checkThirdParty = false },
+       telemetry = { enable = false },
+     },
+}
+require('lspconfig').ccls.setup {
+     on_attach = on_attach,
+     capabilities = capabilities,
+     Lua = {
+       workspace = { checkThirdParty = false },
+       telemetry = { enable = false },
+     },
+}
