4c668c255a87401d3e0f1aba9043aca2dd4e2a55
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2023-09-05 19:59:44 +0100

    steam does not work in hm

--- NixOs/modules/packages.nix
+++ NixOs/modules/packages.nix
@@ -5,6 +5,15 @@
 {
 
     programs = {
+        steam = {
+            enable = true;
+        
+            # Open ports in the firewall for Steam Remote Play
+            remotePlay.openFirewall = true;
+        
+            # Open ports in the firewall for Source Dedicated Server
+            dedicatedServer.openFirewall = true; 
+        };
         # Some programs need SUID wrappers, can be configured further or are
         # started in user sessions.
         # mtr.enable = true;
