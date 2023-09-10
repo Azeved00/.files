aa3cb9ac677a4537939ea731baa39a9dcc41303d
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2023-09-10 23:38:10 +0100

    using my custom sddm theme

--- NixOs/modules/min-sddm-theme.nix
+++ NixOs/modules/min-sddm-theme.nix
@@ -14,10 +14,10 @@ stdenvNoCC.mkDerivation {
     src = fetchFromGitHub {
     owner = "Azeved00";
     repo = "min-sddm";
-    rev = "d38ae1a5c13b6f07ff7fc861ad895b5b19aa7a49";
-    hash = "sha256-aDi9mytqq4F47LZ/sCT17SIGy3tEgUSAqvTyMQitJww=";
+    rev = "c584d06f97b29140957c8cd1e9f63eb6c8176627";
+    hash = "sha256-DQ8OiJFwoFFQuqWIxLdJIuPVEiba6kbzCpamQU+WquA=";
   };
-  
+
     nativeBuildInputs = [
         wrapQtAppsHook
     ];
