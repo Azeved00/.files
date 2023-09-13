00a8d0d1624a135d34f128f6667c41f3acffc186
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2023-09-14 00:12:07 +0100

    added dunst to the configuration

---
+++ home-manager/dunst.nix
@@ -0,0 +1,7 @@
+{...}:
+{
+    services.dunst = {
+        enable = true;
+        configFile = "$HOME/.files/Config/dunst/dunstrc";
+    };
+}
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -18,6 +18,7 @@ in
             ./polybar.nix 
             ./rofi.nix
             ./i3.nix
+            ./dunst.nix
     	] 
     	else []
     );
