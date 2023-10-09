d4aee60bf844a832804dec0d8baf1af2ab5ad952
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2023-10-09 22:39:00 +0100

    hardware-config is now imported in flake file

--- NixOs/configuration.nix
+++ NixOs/configuration.nix
@@ -2,9 +2,6 @@
 {
     imports =
     [ 
-
-        #<home-manager/nixos>
-        
         ./modules/boot.nix
         ./modules/users.nix
         ./modules/network.nix
@@ -14,11 +11,7 @@
         ./modules/local.nix
         ./modules/services.nix
         ./modules/variables.nix
-    ] ++ (if builtins.pathExists "/etc/nixos/hardware-configuration.nix" then 
-    [
-        /etc/nixos/hardware-configuration.nix
-    ] 
-    else []);
+    ] ;
 
 
     nix.gc = {
