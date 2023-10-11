167dd2c5b54f635ec9e0b7293064667eca5d88db
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2023-10-11 15:51:47 +0100

    added home-manager to flake.nix

--- NixOs/flake.nix
+++ NixOs/flake.nix
@@ -3,11 +3,16 @@
 
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	home-manager = {
+	    url = "github:nix-community/home-manager";
+	    inputs.nixpkgs.follows = "nixpkgs";
+	};
     };
 
-    outputs = { self, nixpkgs }: 
+    outputs = { self, nixpkgs, home-manager, ... }: 
     let
         mysystem = "x86_64-linux";
+	pkgs = nixpkgs.legacyPackages.${mysystem};
     in
     {
         nixosConfigurations = {
@@ -21,7 +26,21 @@
                     ./configuration.nix
                 ];
             };
-        };
+	    vm = nixpkgs.lib.nixosSystem {
+                specialArgs = { inherit mysystem; };
+                
+                system = mysystem;
 
+                modules = [
+                    ./hardware-configs/vm.nix
+                    ./configuration.nix
+                ];
+            };
+
+        };
+	homeConfigurations.azevedo = home-manager.lib.homeManagerConfiguration {
+		inherit pkgs;
+		modules = [ ../home-manager/home.nix ];
+	};
     };
 }
