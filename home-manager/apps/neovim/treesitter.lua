bc927223d4a8e480430dd8036249b2f05354beca
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2023-11-07 14:50:45 +0000

    added treesitter config

---
+++ home-manager/apps/neovim/treesitter.lua
@@ -0,0 +1,6 @@
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = "all",  -- Install parsers for all supported languages
+  highlight = {
+    enable = true,  -- Enable syntax highlighting for supported languages
+  },
+}
