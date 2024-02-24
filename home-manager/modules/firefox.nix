06ebff72308ef8e23f8b33d5a18d56462eb8b55d
Azeved00 <jferazevedo@gmail.com>
2024-02-24 16:24:46 +0000

    firefox module-wrong url

--- home-manager/modules/firefox.nix
+++ home-manager/modules/firefox.nix
@@ -64,7 +64,7 @@ in
 
                         "Home-Manager Option Search" = {
                             urls = [{
-                                template = "https://mipmip.github.io/home-manager-option-search/?query=firefox";
+                                template = "https://mipmip.github.io/home-manager-option-search/?";
                                 params = [
                                     { name = "query"; value = "{searchTerms}"; }
                                 ];
