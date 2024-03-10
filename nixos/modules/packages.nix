42b3bdfecd26ed582b0539eba87f1651c53c3fd7
Azeved00 <jferazevedo@gmail.com>
2024-03-10 17:20:56 +0000

    steam is now not default

--- nixos/modules/packages.nix
+++ nixos/modules/packages.nix
@@ -1,15 +1,6 @@
 { pkgs, ... }:
 {
     programs = {
-        steam = {
-            enable = true;
-        
-            # Open ports in the firewall for Steam Remote Play
-            remotePlay.openFirewall = true;
-        
-            # Open ports in the firewall for Source Dedicated Server
-            dedicatedServer.openFirewall = true; 
-        };
         # Some programs need SUID wrappers, can be configured further or are
         # started in user sessions.
         # mtr.enable = true;
--- nixos/profiles/blizzard/default.nix
+++ nixos/profiles/blizzard/default.nix
@@ -10,4 +10,11 @@
 
     dotfiles.nixos.wayland.enable = true;
 
+    programs.steam = {
+        enable = true;
+        # Open ports in the firewall for Steam Remote Play
+        remotePlay.openFirewall = true;
+        # Open ports in the firewall for Source Dedicated Server
+        dedicatedServer.openFirewall = true; 
+    };
 }
