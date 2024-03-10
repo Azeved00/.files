046409071b19167d00f1f31ee3c17e154e586c0d
Azeved00 <jferazevedo@gmail.com>
2024-03-10 20:55:13 +0000

    lf: added zip and unzip commands and updated icons

--- home-manager/modules/lf/default.nix
+++ home-manager/modules/lf/default.nix
@@ -1,4 +1,4 @@
-{config, lib, ...}:
+{config, lib, pkgs, ...}:
 let 
     cfg = config.dotfiles.home-manager.lf;
 in
@@ -28,10 +28,23 @@ in
                 edit = ''$$EDITOR $f'';
                 mkdir = ''$mkdir $*'';
                 touch = ''$touch $*'';
+                unarchive = ''
+                ''${{
+                  case "$f" in
+                      *.zip) ${pkgs.unzip}/bin/unzip "$f" ;;
+                      *.tar.gz) tar -xzvf "$f" ;;
+                      *.tar.bz2) tar -xjvf "$f" ;;
+                      *.tar) tar -xvf "$f" ;;
+                      *) echo "Unsupported format" ;;
+                  esac
+                }}
+                '';
+                zip = ''''$${pkgs.zip}/bin/zip -r -q $1 $fs'';
             };
 
             keybindings = {
                 D = "delete"; 
+                "." = "set hidden!"; 
                 sf = "push :touch<space>";
                 sd = "push :mkdir<space>";
             };
