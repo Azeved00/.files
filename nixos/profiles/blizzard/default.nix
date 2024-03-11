b628cb55acc80d0c975c143d3f9091da5632dd17
Azeved00 <jferazevedo@gmail.com>
2024-03-11 19:01:25 +0000

    blizard profile> added monitor config

--- nixos/profiles/blizzard/default.nix
+++ nixos/profiles/blizzard/default.nix
@@ -17,4 +17,22 @@
         # Open ports in the firewall for Source Dedicated Server
         dedicatedServer.openFirewall = true; 
     };
+
+    services.xserver.xrandrHeads = [
+        {
+            output = "HDMI-1";
+            primary = true;
+            monitorConfig = ''
+                DisplaySize 1920 1080
+            '';
+        }
+        {
+            output = "DP-3";
+            monitorConfig = ''
+                DisplaySize 1920 1080
+                Option "RightOf" "HDMI-1"
+
+            '';
+        }
+    ];
 }
