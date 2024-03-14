d146fbb88c103f46b5b6de98ec5309c64c510bad
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2024-03-14 08:42:47 +0000

    eww bar initial commit

---
+++ home-manager/modules/eww-bar/flake.nix
@@ -0,0 +1,37 @@
+{
+    description = "custom eww module";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+	};
+
+    outputs = { self, ... } @ inputs: 
+    let
+        pkgs = import inputs.nixpkgs { inherit system; };
+        ROOT = builtins.toString ./.;
+        name = "eww development";
+        system = "x86_64-linux";
+    in {
+        devShells."${system}".default = pkgs.mkShell {
+            inherit name ROOT;
+
+            buildInputs = with pkgs; [ eww ];
+
+            shellHook = ''
+                export NIX_SHELL_NAME="${name}" 
+                echo -ne "\033]0;${name}\007"
+
+
+                alias eww="${pkgs.eww}/bin/eww"
+                eww daemon --config ${ROOT}
+
+                alias open="eww open bar --config ${ROOT}"
+                alias kill="eww close-all --config ${ROOT}"
+
+                reload
+                trap "${pkgs.eww}/bin/eww kill --config ${ROOT}" EXIT
+            '';
+
+        };
+    };
+}
