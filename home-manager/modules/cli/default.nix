b4dd2b00b0aacdd1209f4dad10e82a89e9c6ad3c
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2024-04-23 09:17:23 +0100

    user: grouped terminal stuff

---
+++ home-manager/modules/cli/default.nix
@@ -0,0 +1,12 @@
+{
+    imports =  [
+        ./neovim
+        ./tmux
+        ./lf
+        ./bash.nix
+        ./git.nix
+        ./readline.nix
+        ./ssh.nix
+        ./packages.nix
+    ];
+}
--- home-manager/modules/default.nix
+++ home-manager/modules/default.nix
@@ -9,13 +9,6 @@
         ./eww-bar
         ./zathura.nix
 
-        ./neovim
-        ./tmux
-        ./lf
-        ./shell/bash.nix
-        ./shell/git.nix
-        ./shell/readline.nix
-        ./shell/ssh.nix
-        ./shell/packages.nix
+        ./cli
     ];
 }
