41244dbf812d7d415bcb84b52fe0e46337214bb6
Azeved00 <jferazevedo@gmail.com>
2024-05-31 19:03:14 +0100

    dev command now gets into tmux before entering nix shell

--- home-manager/modules/cli/bash/funcs.sh
+++ home-manager/modules/cli/bash/funcs.sh
@@ -3,13 +3,17 @@ function link-proj() {
 }
 
 function dev() {
-    pushd "$1" 
+    if [[ -n "$1" ]] ; then
+        pushd "$1" 
+    else
+        pushd "$PWD" 
+    fi
 
-    #echo "$PWD"
-    local NIX_SHELL_NAME="$(nix develop --impure -c bash -c "env | awk -F'=' '{ if (\$1 == \"name\") print \$2 }'")";
-    tmux new-session -d -s "$NIX_SHELL_NAME";
-    tmux split-window -h -t "$NIX_SHELL_NAME":0 "nix develop --impure";
-    tmux attach -t "$NIX_SHELL_NAME";
+    local TEMP='SOME_TEMPORARY_NAME'
+    tmux new-session -d -s "$TEMP";
+    tmux split-window -h -t "$TEMP":0 "nix develop --impure";
+    tmux send-keys -t "$TEMP":0.0 "tmux rename-session \"\$(nix --quiet develop --quiet -c bash -c \"env | awk -F'=' '{ if (\\\$1 == \\\"name\\\") print \\\$2 }'\")\"" Enter
+    tmux a;
 
     popd
 }
