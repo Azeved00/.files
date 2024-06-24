ba9e5917cdaac4406e77b0eca2246936994eab7a
Azeved00 <jferazevedo@gmail.com>
2024-06-24 20:39:32 +0100

    changed default name and when tmux session is renamed

--- home-manager/modules/cli/bash/funcs.sh
+++ home-manager/modules/cli/bash/funcs.sh
@@ -9,11 +9,13 @@ function dev() {
         pushd "$PWD" 
     fi
 
-    local TEMP='SOME_TEMPORARY_NAME'
+    local TEMP=$(basename "$PWD")
     tmux new-session -d -s "$TEMP";
-    tmux split-window -h -t "$TEMP":0 "nix develop --impure";
-    tmux send-keys -t "$TEMP":0.0 "tmux rename-session \"\$(nix --quiet develop --quiet -c bash -c \"env | awk -F'=' '{ if (\\\$1 == \\\"name\\\") print \\\$2 }'\")\"" Enter
-    tmux a;
+    if [ -f "flake.nix" ]; then
+        tmux split-window -h -t "$TEMP":0 "nix develop --impure";
+        tmux send-keys -t "$TEMP":0.0 "tmux rename-session \"\$(nix --quiet develop --quiet -c bash -c \"env | awk -F'=' '{ if (\\\$1 == \\\"name\\\") print \\\$2 }'\")\"" Enter
+    fi
+    tmux attach-session;
 
     popd
 }
