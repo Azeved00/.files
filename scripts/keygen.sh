3daea7f042d05a67bc6082fc7339dd8848542198
João Azevedo <62391524+Azeved00@users.noreply.github.com>
2024-10-21 08:40:35 +0100

    added script for easily generating ssh keys

---
+++ scripts/keygen.sh
@@ -0,0 +1,25 @@
+echo "------------------- generating key ------------------"
+
+if [ -z "$SSH_AUTH_SOCK" ]; then
+    eval "$(ssh-agent -s)" 
+fi
+
+read -p "Enter your email address: " email
+read -p "Enter the desired SSH key file name (without extension): " key_filename
+
+ssh_key_file="$HOME/.ssh/${key_filename}"
+
+
+if [ -f "$ssh_key_file" ]; then
+    echo "SSH key file '$key_filename' already exists!"
+    read -p "Do you want to proceed and overwrite? (y/n): " overwrite_response
+    if [ "$overwrite_response" != "y" ]; then
+        echo "Aborting."
+        exit 1
+    fi
+fi
+
+ssh-keygen -t ed25519 -C "$email" -f "$ssh_key_file"
+
+ssh-add "$ssh_key_file"
+cat "${ssh_key_file}.pub"
