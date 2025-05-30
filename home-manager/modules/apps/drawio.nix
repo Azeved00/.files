a9c9cb76a1c9cc994b7d0e37ce30905316c3a099
Azeved00 <jferazevedo@gmail.com>
2025-05-30 11:28:23 +0100

    set default apps for missing things@

--- home-manager/modules/apps/drawio.nix
+++ home-manager/modules/apps/drawio.nix
@@ -9,34 +9,6 @@ in
 
     config = lib.mkIf cfg.enable {
         home.packages = with pkgs; [ drawio ];
-
-        xdg = {
-            enable = true;
-            mimeApps.enable = true;
-
-            mimeApps.defaultApplications."application/drawio" = [ "drawio.desktop" ];
-
-            dataFile."mime/packages/drawio.xml"= {
-                enable = true;
-                onChange = "update-mime-database ~/.local/share/mime";
-                text = ''
-                    <?xml version="1.0" encoding="utf-8"?>
-                    <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
-                    <mime-type type="application/vnd.jgraph.mxfile">
-                      <glob pattern="*.drawio"/>
-                        <comment>draw.io Diagram</comment>
-                      <icon name="x-office-document" />
-                    </mime-type>
-                    <mime-type type="application/vnd.visio">
-                      <glob pattern="*.vsdx"/>
-                        <comment>VSDX Document</comment>
-                      <icon name="x-office-document" />
-                    </mime-type>
-                    </mime-info>
-                '';
-            };
-
-        };
     };
 }
 
--- home-manager/modules/desktop/xdg.nix
+++ home-manager/modules/desktop/xdg.nix
@@ -11,6 +11,26 @@ in
         xdg = {
             enable = true;
 
+            dataFile."mime/packages/drawio.xml"= {
+                enable = true;
+                onChange = "update-mime-database ~/.local/share/mime";
+                text = ''
+                    <?xml version="1.0" encoding="utf-8"?>
+                    <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
+                    <mime-type type="application/vnd.jgraph.mxfile">
+                      <glob pattern="*.drawio"/>
+                        <comment>draw.io Diagram</comment>
+                      <icon name="x-office-document" />
+                    </mime-type>
+                    <mime-type type="application/vnd.visio">
+                      <glob pattern="*.vsdx"/>
+                        <comment>VSDX Document</comment>
+                      <icon name="x-office-document" />
+                    </mime-type>
+                    </mime-info>
+                '';
+            };
+
             mimeApps = {
                 enable = true;
                 associations.added = {
@@ -20,17 +40,49 @@ in
                     "text/rust"         =  ["nvim.desktop"];
                     "text/markdown"     =  ["nvim.desktop"];
                     "application/toml"  =  ["nvim.desktop"];
+                    "application/drawio" = [ "drawio.desktop" ];
                 };
                 associations.removed= {
                     "application/pdf"   =  ["krita_pdf.desktop"];
                 };
                 defaultApplications = {
-                    "application/pdf"   =  ["org.pwmt.zathura.desktop"];
+                    # 📄 Text and Code Files
+                    "text/plain"        = ["nvim.desktop"];
+                    "text/rust"         = ["nvim.desktop"];
+                    "text/markdown"     = ["nvim.desktop"];
+                    "application/toml"  = ["nvim.desktop"];
+                    "application/json"  = ["nvim.desktop"];
+                    "application/xml"   = ["nvim.desktop"];
+                    "application/x-yaml"        = ["nvim.desktop"];
+                    "application/x-shellscript" = ["nvim.desktop"];
+                    "text/x-python"     = ["nvim.desktop"];
+                    "text/x-csrc"       = ["nvim.desktop"];
+                    "text/x-c++src"     = ["nvim.desktop"];
+                    "text/html"         = ["nvim.desktop"];
+                    "text/css"          = ["nvim.desktop"];
+                    "application/javascript"    = ["nvim.desktop"];
 
-                    "text/plain"        =  ["nvim.desktop"];
-                    "text/rust"         =  ["nvim.desktop"];
-                    "text/markdown"     =  ["nvim.desktop"];
-                    "application/toml"  =  ["nvim.desktop"];
+                    # 🖼️ Images
+                    "image/jpeg"        = ["feh.desktop"];
+                    "image/png"         = ["feh.desktop"];
+                    "image/gif"         = ["feh.desktop"];
+                    "image/svg+xml"     = ["feh.desktop"];
+
+                    # 🎬 Video
+                    "video/mp4"         = ["vlc.desktop"];
+                    "video/x-matroska"  = ["vlc.desktop"];
+                    "video/webm"        = ["vlc.desktop"];
+
+                    # 🧾 Documents
+                    "application/pdf"   =  ["org.pwmt.zathura.desktop"];
+                    "application/drawio" = [ "drawio.desktop" ];
+
+                    # 🌐 Web/URI Handlers
+                    "x-scheme-handler/http"     = ["zen.desktop"];
+                    "x-scheme-handler/https"    = ["zen.desktop"];
+                    "x-scheme-handler/ftp"      = ["zen.desktop"];
+                    "x-scheme-handler/mailto"   = ["zen.desktop"];
+                    "x-scheme-handler/magnet"   = ["zen.desktop"];
                 };
             };
 
